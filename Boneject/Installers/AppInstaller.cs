0983b2960d3aa85937348749f7092da4f2be03a9
Millzy <millzyyt08@gmail.com>
2023-04-03 11:50:55 +0100

    basic installer, will add more as I need them

---
+++ Boneject/Installers/AppInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Boneject.Installers
+{
+    public class AppInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
---
+++ Boneject/Installers/MenuInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Boneject.Installers
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
