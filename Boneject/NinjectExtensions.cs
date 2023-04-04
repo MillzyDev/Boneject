58041ebdc3c796efd6ec5515237072ed9685dc40
Millzy <millzyyt08@gmail.com>
2023-04-04 09:11:01 +0100

    these cool methods for unity components

---
+++ Boneject/NinjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Ninject.Syntax;
+using UnityEngine;
+
+namespace Boneject;
+
+public static class NinjectExtensions
+{
+    public static IBindingWhenInNamedWithOrOnSyntax<T> AsComponentOnNewGameObject<T>(this IBindingToSyntax<T> self) where T : Component
+    {
+        var go = new GameObject(typeof(T).FullName);
+        var instance = go.AddComponent<T>();
+        return self.ToConstant(instance);
+    }
+
+    public static IBindingWhenInNamedWithOrOnSyntax<T> AsComponentOnExistingGameObject<T>(
+        this IBindingToSyntax<T> self, GameObject gameObject) where T : Component
+    {
+        var instance = gameObject.AddComponent<T>();
+        return self.ToConstant(instance);
+    }
+}
