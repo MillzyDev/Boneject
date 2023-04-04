ea6d6403ac2facf1daf54e3e2d86f7d6eaf4e03c
Millzy <millzyyt08@gmail.com>
2023-04-04 19:05:46 +0100

    this thing

---
+++ Boneject/ModuleSet.cs
@@ -0,0 +1,13 @@
+﻿namespace Boneject;
+
+internal struct ModuleSet
+{
+    public readonly Type moduleType;
+    public readonly object[]? initialParameters;
+
+    public ModuleSet(Type moduleType, object[]? initialParameters = null)
+    {
+        this.moduleType = moduleType;
+        this.initialParameters = initialParameters;
+    }
+}
