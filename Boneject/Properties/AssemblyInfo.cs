f5d1f7097bc193a17f366578abf8ddf48184fafe
Millzy <millzyyt08@gmail.com>
2023-04-04 09:10:13 +0100

    aaaah

---
+++ Boneject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Boneject;
+using MelonLoader;
+using BuildInfo = Boneject.Properties.BuildInfo;
+
+[assembly: AssemblyTitle("Boneject")]
+[assembly: AssemblyDescription("A basic Ninject implementation and wrapper for BONELAB.")]
+[assembly: AssemblyProduct("Boneject")]
+[assembly: AssemblyCopyright("Copyright © Millzy 2023")]
+
+[assembly: AssemblyVersion(BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(null!)]
+[assembly: AssemblyTrademark(null!)]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(Mod), "Boneject", BuildInfo.Version, "Millzy")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
