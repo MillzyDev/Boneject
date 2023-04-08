e5f2ff5b10f2224bc852bddc78371a78b0efd70c
Millzy <millzyyt08@gmail.com>
2023-04-08 13:30:03 +0100

    should work

---
+++ Boneject/ModuleLoaders/ModuleLoader.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using Ninject.Modules;
+
+namespace Boneject.ModuleLoaders;
+
+public class ModuleLoader
+{
+    private List<INinjectModule> _modules;
+
+    protected ModuleLoader()
+    {
+        _modules = new List<INinjectModule>();
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public StandardKernel? Kernel { get; private set; }
+
+    public void RegisterModule(INinjectModule module)
+    {
+        _modules.Add(module);
+    }
+
+    public void BeginLoad()
+    {
+        Kernel = new StandardKernel();
+        
+        foreach (var dependency in GlobalDependencies.Get())
+        {
+            if (dependency.Value == null)
+                Kernel.Bind(dependency.Key).ToSelf().InSingletonScope();
+            else
+                Kernel.Bind(dependency.Key).ToConstant(dependency.Value).InSingletonScope();
+        }
+        
+        Kernel.Load(_modules.ToArray());
+    }
+}
