5d3e61cd291475dc92f45d0ebcba906a55842f34
Millzy <millzyyt08@gmail.com>
2023-04-08 13:29:35 +0100

    working bonejector (i hope)

---
+++ Boneject/Bonejector.cs
@@ -0,0 +1,51 @@
+﻿using Boneject.ModuleLoaders;
+using Ninject.Modules;
+
+namespace Boneject;
+
+public sealed class Bonejector
+{
+    private static readonly Lazy<Bonejector> Lazy = new(() => new Bonejector());
+
+    private readonly Dictionary<Type, HashSet<INinjectModule>> _modules = new();
+
+    private Bonejector()
+    {
+    }
+
+    public static Bonejector Instance => Lazy.Value;
+
+    public void InstallModule<T>(Location location) where T : INinjectModule
+    {
+        var module = Activator.CreateInstance<T>();
+        var loaderTypes = LoadersForLocation(location);
+        foreach (var type in loaderTypes)
+        {
+            if (_modules.ContainsKey(type))
+                _modules.Add(type, new HashSet<INinjectModule>());
+            _modules[type].Add(module);
+        }
+    }
+
+    public HashSet<INinjectModule> ModulesForLoader<T>() where T : ModuleLoaders.ModuleLoader
+    {
+        return _modules[typeof(T)];
+    }
+
+    public HashSet<INinjectModule> ModulesForLoader(Type loaderType)
+    {
+        if (!typeof(ModuleLoaders.ModuleLoader).IsAssignableFrom(loaderType))
+            throw new ArgumentException("loaderType is not assignable from Boneject.ModuleLoaders.ModuleLoader");
+        return _modules[loaderType];
+    }
+
+    private IEnumerable<Type> LoadersForLocation(Location location)
+    {
+        HashSet<Type> moduleLoaders = new();
+
+        if (location.HasFlag(Location.App))
+            moduleLoaders.Add(typeof(AppModuleLoader));
+
+        return moduleLoaders;
+    }
+}
--- Boneject/GlobalDependencies.cs
+++ Boneject/GlobalDependencies.cs
@@ -1,21 +1,23 @@
 ﻿namespace Boneject;
 
-public static class GlobalDependencyContainer
+public static class GlobalDependencies
 {
-    private static HashSet<KeyValuePair<Type, object?>> _globalDependencies = new();
+    private static Dictionary<Type, object?> _globalDependencies = new();
 
     public static void AddDependency<T>() where T : class
         => AddDependency<T>(null);
 
     public static void AddDependency(Type type, object? dependency = null)
-        => _globalDependencies.Add(new KeyValuePair<Type, object?>(type, dependency));
+        => _globalDependencies.Add(type, dependency);
     
     public static void AddDependency<T>(T? dependency)
-        => _globalDependencies.Add(new KeyValuePair<Type, object?>(typeof(T), dependency));
+        => _globalDependencies.Add(typeof(T), dependency);
 
     public static object? GetDependency(Type type)
         => _globalDependencies.FirstOrDefault(x => x.Key == type).Value;
 
     public static T? GetDependency<T>()
         => (T?) _globalDependencies.FirstOrDefault(x => x.Key == typeof(T)).Value;
+    
+    public static Dictionary<Type, object?> Get
 }
