f98c8335b993ce74d811962c32fee46efc2c03eb
Millzy <millzyyt08@gmail.com>
2023-04-08 13:30:30 +0100

    naming con

--- Boneject/GlobalDependencies.cs
+++ Boneject/GlobalDependencies.cs
@@ -2,22 +2,23 @@
 
 public static class GlobalDependencies
 {
-    private static Dictionary<Type, object?> _globalDependencies = new();
+    private static readonly Dictionary<Type, object?> Dependencies = new();
 
     public static void AddDependency<T>() where T : class
         => AddDependency<T>(null);
 
     public static void AddDependency(Type type, object? dependency = null)
-        => _globalDependencies.Add(type, dependency);
+        => Dependencies.Add(type, dependency);
     
     public static void AddDependency<T>(T? dependency)
-        => _globalDependencies.Add(typeof(T), dependency);
+        => Dependencies.Add(typeof(T), dependency);
 
     public static object? GetDependency(Type type)
-        => _globalDependencies.FirstOrDefault(x => x.Key == type).Value;
+        => Dependencies.FirstOrDefault(x => x.Key == type).Value;
 
     public static T? GetDependency<T>()
-        => (T?) _globalDependencies.FirstOrDefault(x => x.Key == typeof(T)).Value;
-    
-    public static Dictionary<Type, object?> Get
+        => (T?) Dependencies.FirstOrDefault(x => x.Key == typeof(T)).Value;
+
+    public static Dictionary<Type, object?> Get()
+        => Dependencies;
 }
