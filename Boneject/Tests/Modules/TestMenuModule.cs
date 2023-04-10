ab887e92da3223268ba6bdbce604b0f3769455cc
Millzy <millzyyt08@gmail.com>
2023-04-10 15:05:55 +0100

    test menu

---
+++ Boneject/Tests/Modules/TestMenuModule.cs
@@ -0,0 +1,17 @@
+﻿#if ENABLE_TESTS
+using MelonLoader;
+using Ninject.Modules;
+
+namespace Boneject.Tests.Modules;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class TestMenuModule : NinjectModule
+{
+    public override void Load()
+    {
+        MelonLogger.Msg("Loaded Module!");
+        Bind<TestMenuDependency>().ToSelf().InSingletonScope();
+        this.ResolveAll();
+    }
+}
+#endif
---
+++ Boneject/Tests/TestMenuDependency.cs
@@ -0,0 +1,26 @@
+﻿#if ENABLE_TESTS
+using Il2CppSLZ.Bonelab;
+using MelonLoader;
+using Ninject;
+
+namespace Boneject.Tests;
+
+public class TestMenuDependency : IInitializable
+{
+    private readonly GameControl_startup _startup;
+    private readonly TestAppDependency _appDependency;
+    
+    [Inject]
+    private TestMenuDependency(GameControl_startup startup, TestAppDependency appDependency)
+    {
+        _startup = startup;
+        _appDependency = appDependency;
+    }
+    
+    public void Initialize()
+    {
+        MelonLogger.Msg($"Value of startup GameControl is: {_startup}");
+        MelonLogger.Msg(_appDependency.str);
+    }
+}
+#endif
