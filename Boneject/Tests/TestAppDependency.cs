6e095fe779e0267d850b48ef0a0cab19c30cc0f3
Millzy <millzyyt08@gmail.com>
2023-04-10 15:05:59 +0100

    dis amb

--- Boneject/Tests/TestAppDependency.cs
+++ Boneject/Tests/TestAppDependency.cs
@@ -2,7 +2,7 @@
 namespace Boneject.Tests;
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class TestDependency
+public class TestAppDependency
 {
     // ReSharper disable once ConvertToConstant.Global
     public readonly string str = "Hello, world!";
--- Boneject/Tests/TestAppLogger.cs
+++ Boneject/Tests/TestAppLogger.cs
@@ -4,9 +4,9 @@ using MelonLoader;
 namespace Boneject.Tests;
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class TestLogger
+public class TestAppLogger
 {
-    public TestLogger(TestDependency dependency)
+    public TestAppLogger(TestAppDependency dependency)
     {
         MelonLogger.Msg(dependency.str);
         MelonLogger.Msg("If you are reading this, and you are not developing for Boneject, please inform Millzy; this build has the wrong config.");
