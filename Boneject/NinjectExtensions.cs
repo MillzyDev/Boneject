6c83d8be6fb870ac85b19b219ae69fcf4a4e5095
Millzy <millzyyt08@gmail.com>
2023-04-10 14:21:06 +0100

    get all dependencies extension method

--- Boneject/NinjectExtensions.cs
+++ Boneject/NinjectExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Ninject.Syntax;
+﻿using System.Reflection;
+using Ninject;
+using Ninject.Syntax;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Boneject;
 
@@ -18,5 +21,16 @@ public static class NinjectExtensions
     {
         var instance = gameObject.AddComponent<T>();
         return self.ToConstant(instance);
+    }
+
+    public static Dictionary<Type, object> GetAll(this StandardKernel self)
+    {
+        // We have to get all the binding types from the container, there are no public methods to do this,
+        // so we use reflection to get all the bindings and their associated types.
+        var bindingCacheFieldInfo = typeof(StandardKernel).GetField("bindingCache", BindingFlags.NonPublic | BindingFlags.Instance);
+        var bindingCache = (bindingCacheFieldInfo?.GetValue(self) as Dictionary<Type, IBinding[]>)!;
+        
+        // Create a dictionary of types and their resolved instances.
+        return bindingCache.ToDictionary(binding => binding.Key, binding => self.Get(binding.Key));
     }
 }
