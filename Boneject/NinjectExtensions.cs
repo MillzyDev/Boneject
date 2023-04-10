bd8d60e328504b72c8bdd07c189c3332aa4d969a
Millzy <millzyyt08@gmail.com>
2023-04-10 15:06:12 +0100

    buggy resolve all

--- Boneject/NinjectExtensions.cs
+++ Boneject/NinjectExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
+using MelonLoader;
 using Ninject;
+using Ninject.Modules;
 using Ninject.Syntax;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -23,14 +25,21 @@ public static class NinjectExtensions
         return self.ToConstant(instance);
     }
 
-    public static Dictionary<Type, object> GetAll(this StandardKernel self)
+    public static Dictionary<Type, object>? GetAll(this KernelBase self)
     {
         // We have to get all the binding types from the container, there are no public methods to do this,
         // so we use reflection to get all the bindings and their associated types.
-        var bindingCacheFieldInfo = typeof(StandardKernel).GetField("bindingCache", BindingFlags.NonPublic | BindingFlags.Instance);
-        var bindingCache = (bindingCacheFieldInfo?.GetValue(self) as Dictionary<Type, IBinding[]>)!;
+        var bindingCacheFieldInfo = typeof(KernelBase).GetField("bindingCache", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        var bindingCache = bindingCacheFieldInfo?.GetValue(self) as Dictionary<Type, IBinding[]>;
+        MelonLogger.Msg(bindingCache == null); 
         
         // Create a dictionary of types and their resolved instances.
-        return bindingCache.ToDictionary(binding => binding.Key, binding => self.Get(binding.Key));
+        return bindingCache?.ToDictionary(binding => binding.Key, binding => self.Get(binding.Key));
+    }
+
+    public static void ResolveAll(this INinjectModule self)
+    {
+        var kernel = self.Kernel as KernelBase;
+        kernel?.GetAll();
     }
 }
