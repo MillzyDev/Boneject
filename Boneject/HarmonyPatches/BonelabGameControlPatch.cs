ac906717f6c9ba958008a3b5cb20af5200dae6df
Millzy <millzyyt08@gmail.com>
2023-04-23 10:30:47 +0100

    those 2 new locs

---
+++ Boneject/HarmonyPatches/BonelabGameControlPatch.cs
@@ -0,0 +1,22 @@
+﻿using Boneject.ModuleLoaders;
+using HarmonyLib;
+using MelonLoader;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(BonelabGameControl))]
+[HarmonyPatch(nameof(BonelabGameControl.Start))]
+internal static class GameControlPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void Postfix(BonelabGameControl __instance)
+    {
+        MelonLogger.Msg($"Loading modules for location: {InstallLocation.Game}");
+        var moduleLoader = new GameModuleLoader();
+        moduleLoader.Kernel?.Bind<BonelabGameControl>().ToConstant(__instance).InSingletonScope();
+        moduleLoader.BeginLoad();
+    }
+}
