826d0ce7a8019794d6a12e705102fc34f9d76613
Millzy <millzyyt08@gmail.com>
2023-04-25 08:14:51 +0100

    needed to deep clone

---
+++ Boneject/BonejectKernel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Ninject;
+
+namespace Boneject;
+
+[Serializable]
+public class BonejectKernel : StandardKernel
+{
+    public BonejectKernel DeepClone()
+    {
+        using var memStream = new MemoryStream();
+        
+        var formatter = new BinaryFormatter();
+        formatter.Serialize(memStream, this);
+        memStream.Position = 0;
+        
+        return (BonejectKernel)formatter.Deserialize(memStream);
+    }
+}
--- Boneject/Context.cs
+++ Boneject/Context.cs
@@ -11,22 +11,32 @@ public enum InstallLocation
     None = 0,
     
     /// <summary>
+    /// 
+    /// </summary>
+    App = 1,
+    
+    /// <summary>
     /// Indicated modules should be loaded at <see cref="SLZ.UI.LoadingScene.Start()"/>. The loading screen between levels.
     /// </summary>
-    Loading = 1,
+    Loading = 2,
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    Hub = 4,
     
     /// <summary>
     /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.BonelabGameControl.Start()"/>. This should be every level, except the 2 main menu levels.
     /// </summary>
-    Game = 2,
+    Game = 8,
 
     /// <summary>
     /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_startup.Start()"/>. The first main menu scene.
     /// </summary>
-    MenuStartup = 4,
+    MenuStartup = 16,
     
     /// <summary>
     /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_MenuVoidG114.Start()"/>. The last main menu scene.
     /// </summary>
-    MenuVoidG114 = 8,
+    MenuVoidG114 = 32,
 }
