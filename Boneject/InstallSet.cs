39ea1de3cc3e2f7c3b3d3a6f2cfd847e595c076b
Millzy <millzyyt08@gmail.com>
2023-04-25 19:16:39 +0100

    install set struct

---
+++ Boneject/InstallSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Boneject;
+
+public struct InstallSet
+{
+    public readonly Type moduleType;
+    public readonly IInstallFilter installFilter;
+
+    public readonly object[]? initialParameters;
+
+    public InstallSet(Type moduleType, IInstallFilter installFilter, object[]? initialParameters)
+    {
+        this.moduleType = moduleType;
+        this.installFilter = installFilter;
+        this.initialParameters = initialParameters;
+    }
+}
