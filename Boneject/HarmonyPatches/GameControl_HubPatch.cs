92066c6a72b826060886582fc6253cee86aa2371
Millzy <millzyyt08@gmail.com>
2023-04-25 08:36:38 +0100

    hub patch

---
+++ Boneject/HarmonyPatches/GameControl_HubPatch.cs
@@ -0,0 +1,22 @@
+﻿using Boneject.ModuleLoaders;
+using HarmonyLib;
+using MelonLoader;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(GameControl_Hub))]
+[HarmonyPatch(nameof(GameControl_Hub.Start))]
+// ReSharper disable once InconsistentNaming
+internal static class GameControl_HubPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    private static void Postfix(GameControl_Hub __instance)
+    {
+        MelonLogger.Msg($"Loading modules for context: {Context.Game}");
+        var moduleLoader = new HubModuleLoader();
+        moduleLoader.Kernel?.Bind<GameControl_Hub>().ToConstant(__instance).InSingletonScope();
+        moduleLoader.BeginLoad();
+    }
+}
