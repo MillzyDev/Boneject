9e4b64d9143578fb345e7422b684fd34acb6f11c
Millzy <millzyyt08@gmail.com>
2023-04-25 17:26:29 +0100

    more docs

--- Boneject/Context.cs
+++ Boneject/Context.cs
@@ -11,32 +11,38 @@ public enum Context
     None = 0,
     
     /// <summary>
-    /// 
+    /// Indicates modules should be loaded on application start (when the first Start() method runs).
+    /// All dependencies bound in this context will be available in all other contexts.
     /// </summary>
     App = 1,
     
     /// <summary>
-    /// Indicated modules should be loaded at <see cref="SLZ.UI.LoadingScene.Start()"/>. The loading screen between levels.
+    /// Indicates modules should be loaded at <see cref="SLZ.UI.LoadingScene"/>.
+    /// The loading screen between levels.
     /// </summary>
     Loading = 2,
     
     /// <summary>
-    /// 
+    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_Hub"/>.
+    /// The "02 - BONELAB Hub" level.
     /// </summary>
     Hub = 4,
     
     /// <summary>
-    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.BonelabGameControl.Start()"/>. This should be every level, except the 2 main menu levels.
+    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.BonelabGameControl"/>.
+    /// This should be every level, except the 2 main menu levels and the hub.
     /// </summary>
     Game = 8,
 
     /// <summary>
-    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_startup.Start()"/>. The first main menu scene.
+    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_startup"/>.
+    /// The first main menu scene.
     /// </summary>
     MenuStartup = 16,
     
     /// <summary>
-    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_MenuVoidG114.Start()"/>. The last main menu scene.
+    /// Indicates modules should be loaded at <see cref="SLZ.Bonelab.GameControl_MenuVoidG114.Start()"/>.
+    /// The last main menu scene.
     /// </summary>
     MenuVoidG114 = 32,
 }
