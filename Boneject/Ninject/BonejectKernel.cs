4cee631159585cdef3aa714ec6b76337e7403caf
Millzy <millzyyt08@gmail.com>
2023-04-25 23:43:11 +0100

    basic custom kernel

---
+++ Boneject/Ninject/BonejectKernel.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+using Ninject.Modules;
+
+namespace Boneject.Ninject;
+
+public class BonejectKernel : StandardKernel
+{
+    public BonejectKernel(params INinjectModule[] modules) : base(modules)
+    {
+    }
+
+    public BonejectKernel(INinjectSettings settings, params INinjectModule[] modules) : base(settings, modules)
+    {
+    }
+}
