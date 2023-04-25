a2351800a4f947137c420c85be3e5d2fcffe3fad
Millzy <millzyyt08@gmail.com>
2023-04-25 23:43:50 +0100

    basic mod

---
+++ Boneject/Mod.cs
@@ -0,0 +1,23 @@
+﻿using Boneject.MelonLoader;
+using Boneject.MelonLoader.Attributes;
+using MelonLoader;
+
+namespace Boneject;
+
+public static class BuildInfo
+{
+    public const string name = "Boneject";
+    public const string author = "Millzy";
+    public const string company = null!;
+    public const string version = "1.0.0";
+    public const string downloadLink = null!;
+}
+
+public class Mod : InjectableMelonMod
+{
+    [OnInitialize]
+    void OnInitializeMod()
+    {
+        
+    }
+}
--- Boneject/Properties/AssemblyInfo.cs
+++ Boneject/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using Boneject;
+using MelonLoader;
 using BuildInfo = Boneject.BuildInfo;
 
 [assembly: AssemblyTitle(BuildInfo.name)]
@@ -6,4 +10,15 @@ using BuildInfo = Boneject.BuildInfo;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany(BuildInfo.company)]
 [assembly: AssemblyProduct(BuildInfo.name)]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("""Copyright (c) 2023 Frederick ("Millzy") Mills""")]
+[assembly: AssemblyTrademark(null)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(BuildInfo.version)]
+[assembly: AssemblyFileVersion(BuildInfo.version)]
+[assembly: NeutralResourcesLanguage("en")]
+
+// ReSharper disable once RedundantArgumentDefaultValue
+[assembly: MelonInfo(typeof(Mod), BuildInfo.name, BuildInfo.version, BuildInfo.author, BuildInfo.downloadLink)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonPriority(-1000000)] // -1,000,000
