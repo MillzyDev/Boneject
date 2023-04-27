8519797614dcbacc79d4401550fc13eecdb9c0b9
Millzy <millzyyt08@gmail.com>
2023-04-28 00:30:14 +0100

    deep clone for the kernel

--- Boneject/Ninject/BonejectKernel.cs
+++ Boneject/Ninject/BonejectKernel.cs
@@ -1,4 +1,7 @@
-﻿using Ninject;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Ninject;
 using Ninject.Modules;
 
 namespace Boneject.Ninject;
@@ -12,4 +15,17 @@ public class BonejectKernel : StandardKernel
     public BonejectKernel(INinjectSettings settings, params INinjectModule[] modules) : base(settings, modules)
     {
     }
+
+    private object DeepClone()
+    {
+        using var memStream = new MemoryStream();
+        
+        var formatter = new BinaryFormatter();
+        formatter.Serialize(memStream, this);
+        memStream.Position = 0;
+        
+        return formatter.Deserialize(memStream);
+    }
+
+    public BonejectKernel Clone() => (BonejectKernel)DeepClone();
 }
