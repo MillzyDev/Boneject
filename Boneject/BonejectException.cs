4c355037592712c7af89614a714b2ecd5c841140
Millzy <millzyyt08@gmail.com>
2023-04-27 08:04:27 +0100

    thing to get thrown when something goes wrong when prep

---
+++ Boneject/BonejectException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Boneject;
+
+public class BonejectException : Exception
+{
+    public BonejectException() : base()
+    {
+    }
+
+    public BonejectException(string message) : base(message)
+    {
+    }
+
+    public BonejectException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
