c6d8319334fea8db29ac678f5962b6c1c2dfa477
Millzy <millzyyt08@gmail.com>
2023-04-27 22:30:58 +0100

    init injection test

--- Boneject.Tests/Mod.cs
+++ Boneject.Tests/Mod.cs
@@ -1,9 +1,16 @@
 ﻿using Boneject.MelonLoader;
+using Boneject.MelonLoader.Attributes;
+using Boneject.Ninject;
 
 namespace Boneject.Tests
 {
     public class Mod : InjectableMelonMod
     {
-        
+        [OnInitialize]
+        public void OnInitializeMod(Bonejector bonejector)
+        {
+            LoggerInstance.Msg("Starting Boneject tests.");
+            LoggerInstance.Msg($"Injected Bonejector instance is {NullString.Create(bonejector)}");
+        }
     }
 }
---
+++ Boneject.Tests/NullString.cs
@@ -0,0 +1,6 @@
+﻿namespace Boneject.Tests;
+
+internal static class NullString
+{
+    public static string Create(object? obj) => obj == null ? "NULL" : "NOT NULL";
+}
