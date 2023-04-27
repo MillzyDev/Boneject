e982215ae13fbc1573710fd045da9833728adf77
Millzy <millzyyt08@gmail.com>
2023-04-28 00:29:42 +0100

    part that does the module loading

---
+++ Boneject/Ninject/ModuleLoadExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Boneject.Ninject.Infrastructure;
+using Ninject;
+using Ninject.Modules;
+
+namespace Boneject.Ninject;
+
+public static class ModuleLoadExtensions
+{
+    public static void Load(this IKernel kernel, Type moduleType, params object?[]? args)
+    {
+        Ensure.ArgumentNotNull(kernel, nameof(kernel));
+        if (!moduleType.IsSubclassOf(typeof(INinjectModule)))
+            throw new BonejectException("Cannot load type that does not inherit INinjectModule");
+        
+        var module = (INinjectModule)Activator.CreateInstance(moduleType, BindingFlags.Public | BindingFlags.NonPublic, args);
+        kernel.Load(module);
+    }
+}
