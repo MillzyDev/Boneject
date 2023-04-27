6c1d5fee849003bf3de6433f43c8f50d3b35dc07
Millzy <millzyyt08@gmail.com>
2023-04-28 00:31:40 +0100

    hopefully working app context

--- Boneject/Ninject/Modules/AppModule.cs
+++ Boneject/Ninject/Modules/AppModule.cs
@@ -1,6 +1,19 @@
-﻿namespace Boneject.Ninject.Modules;
+﻿using Ninject.Modules;
 
-public class AppModule
+namespace Boneject.Ninject.Modules;
+
+public class AppModule : NinjectModule
 {
+    private readonly BonejectManager _bonejectManager;
     
+    public AppModule()
+    {
+        _bonejectManager = Mod.BonejectManager;
+    }
+    
+    public override void Load()
+    {
+        _bonejectManager.LoadForContext(this);
+    }
+
 }
