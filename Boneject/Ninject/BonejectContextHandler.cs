45bb212d57a7aec2b5b32a4b6506dc8f6ad18951
Millzy <millzyyt08@gmail.com>
2023-04-28 18:38:11 +0100

    better way to do the shitty deep clone

---
+++ Boneject/Ninject/BonejectContextHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using MelonLoader;
+using Ninject.Modules;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Boneject.Ninject;
+
+[RegisterTypeInIl2Cpp]
+internal class BonejectContextHandler : MonoBehaviour
+{
+    private BonejectManager? _bonejectManager;
+
+    public BonejectContextHandler(IntPtr ptr) : base(ptr)
+    {
+    }
+
+    internal BonejectManager BonejectManager
+    {
+        get => _bonejectManager!;
+        set => _bonejectManager ??= value;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // Persist between scenes
+    }
+
+    private void Start() // Ensure _bonejectManager has a value;
+    {
+        SceneManager.sceneLoaded += DelegateSupport.ConvertDelegate<UnityAction<Scene, LoadSceneMode>>(OnSceneLoaded);
+    }
+
+    private void OnSceneLoaded(Scene _, LoadSceneMode __) => UnloadNonAppBindings();
+
+    private void UnloadNonAppBindings()
+    {
+        var kernel = BonejectManager.Kernel;
+        
+        // Unregister modules
+        IEnumerable<INinjectModule> modules =
+            kernel.GetModules().Where(module => !BonejectManager.PreservedModules.Contains(module));
+        foreach (var module in modules)
+            kernel.Unload(module.Name);
+
+        // Remove bindings
+        var bindings = BonejectManager.BindingCache.Values.SelectMany(value => value)
+            .Where(binding => !BonejectManager.PreservedBindings.Contains(binding));
+        foreach (var binding in bindings)
+            kernel.RemoveBinding(binding);
+    }
+}
--- Boneject/Ninject/BonejectKernel.cs
+++ Boneject/Ninject/BonejectKernel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using Ninject;
+﻿using Ninject;
 using Ninject.Modules;
 
 namespace Boneject.Ninject;
@@ -15,17 +12,4 @@ public class BonejectKernel : StandardKernel
     public BonejectKernel(INinjectSettings settings, params INinjectModule[] modules) : base(settings, modules)
     {
     }
-
-    private object DeepClone()
-    {
-        using var memStream = new MemoryStream();
-        
-        var formatter = new BinaryFormatter();
-        formatter.Serialize(memStream, this);
-        memStream.Position = 0;
-        
-        return formatter.Deserialize(memStream);
-    }
-
-    public BonejectKernel Clone() => (BonejectKernel)DeepClone();
 }
