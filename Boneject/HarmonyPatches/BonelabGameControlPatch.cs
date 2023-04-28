a95bd6441227049cb6cf8ba9a68b71f3724b0c21
Millzy <millzyyt08@gmail.com>
2023-04-28 23:10:19 +0100

    patches for 2 modules to work

---
+++ Boneject/HarmonyPatches/BonelabGameControlPatch.cs
@@ -0,0 +1,26 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using Ninject;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(BonelabGameControl))]
+[HarmonyPatch(nameof(BonelabGameControl.Start))]
+public static class BonelabGameControlPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void PostFix(BonelabGameControl __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+        
+        var saveFeatures = __instance.SaveFeatures;
+        var inventorySaveFilter = __instance.InventorySaveFilter;
+
+        var baseModule = new BonelabModule(bonejectManager, __instance, saveFeatures, inventorySaveFilter);
+        kernel.Load(baseModule);
+    }
+}
---
+++ Boneject/HarmonyPatches/RigManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using Ninject;
+using SLZ.Rig;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(RigManager))]
+[HarmonyPatch(nameof(RigManager.Start))]
+public static class RigManagerPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void PostFix(RigManager __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+
+        var baseModule = new PlayerModule(bonejectManager, __instance);
+        kernel.Load(baseModule);
+    }
+}
