f731d2a0c8cfc4c810e98e99d5b6881e1f545dd2
Millzy <millzyyt08@gmail.com>
2023-04-29 10:29:47 +0100

    fix typo

--- Boneject/HarmonyPatches/RigManagerPatch.cs
+++ Boneject/HarmonyPatches/RigManagerPatch.cs
@@ -1,5 +1,6 @@
 ﻿using Boneject.Ninject.Modules;
 using HarmonyLib;
+using MelonLoader;
 using Ninject;
 using SLZ.Rig;
 
@@ -12,12 +13,14 @@ public static class RigManagerPatch
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
     // ReSharper disable once UnusedMember.Local
-    private static void PostFix(RigManager __instance)
+    private static void Postfix(RigManager __instance)
     {
         var bonejectManager = Mod.BonejectManager;
         var kernel = bonejectManager.Kernel;
 
         var baseModule = new PlayerModule(bonejectManager, __instance);
         kernel.Load(baseModule);
+        
+        MelonLogger.Msg("Player context loaded.");
     }
 }
