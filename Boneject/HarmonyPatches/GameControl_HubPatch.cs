fb637ff4d9950680086e95395f1f3e630a2c8ef2
Millzy <millzyyt08@gmail.com>
2023-04-29 12:27:51 +0100

    hub

---
+++ Boneject/HarmonyPatches/GameControl_HubPatch.cs
@@ -0,0 +1,29 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using Ninject;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(GameControl_Hub))]
+[HarmonyPatch(nameof(GameControl_Hub.Start))]
+public static class GameControl_HubPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void Postfix(GameControl_Hub __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+
+        var rigManager = __instance.rm;
+        var controlPlayer = __instance.controlPlayer;
+        var gauntletElevator = __instance.gauntletElevator;
+        var inventorySaveFilter = __instance.inventorySaveFilter;
+
+        var baseModule = new HubModule(bonejectManager, __instance, rigManager, controlPlayer, gauntletElevator,
+            inventorySaveFilter);
+        kernel.Load(baseModule);
+    }
+}
--- Boneject/Ninject/Modules/HubModule.cs
+++ Boneject/Ninject/Modules/HubModule.cs
@@ -1,11 +1,38 @@
 ﻿using Ninject.Modules;
+using SLZ.Bonelab;
+using SLZ.Rig;
+using SLZ.SaveData;
 
 namespace Boneject.Ninject.Modules;
 
 public class HubModule : NinjectModule
 {
+    private readonly BonejectManager _bonejectManager;
+    private readonly GameControl_Hub _gameControlHub;
+    private readonly RigManager _rigManager;
+    private readonly Control_Player _controlPlayer;
+    private readonly GauntletElevator _gauntletElevator;
+    private readonly InventorySaveFilter _inventorySaveFilter;
+    
+    public HubModule(BonejectManager bonejectManager, GameControl_Hub gameControlHub, RigManager rigManager,
+        Control_Player controlPlayer, GauntletElevator gauntletElevator, InventorySaveFilter inventorySaveFilter)
+    {
+        _bonejectManager = bonejectManager;
+        _gameControlHub = gameControlHub;
+        _rigManager = rigManager;
+        _controlPlayer = controlPlayer;
+        _gauntletElevator = gauntletElevator;
+        _inventorySaveFilter = inventorySaveFilter;
+    }
+    
     public override void Load()
     {
-        throw new System.NotImplementedException();
+        Bind<GameControl_Hub>().ToConstant(_gameControlHub).InSingletonScope();
+        Bind<RigManager>().ToConstant(_rigManager).InSingletonScope();
+        Bind<Control_Player>().ToConstant(_controlPlayer).InSingletonScope();
+        Bind<GauntletElevator>().ToConstant(_gauntletElevator).InSingletonScope();
+        Bind<InventorySaveFilter>().ToConstant(_inventorySaveFilter).InSingletonScope();
+        
+        _bonejectManager.LoadForContext(this);
     }
 }
