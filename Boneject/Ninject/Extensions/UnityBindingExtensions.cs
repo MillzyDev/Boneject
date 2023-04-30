77d824c2aa69303ba7d2a33145adc6c5f776b3e5
Millzy <millzyyt08@gmail.com>
2023-04-30 13:08:21 +0100

    BindMonoBehaviourOnNewGameObject methods

---
+++ Boneject/Ninject/Extensions/UnityBindingExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Syntax;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+namespace Boneject.Ninject.Extensions;
+
+// ReSharper disable once UnusedType.Global
+public static class UnityBindingExtensions
+{
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnNewGameObject<T>(this IKernel self)
+        where T : MonoBehaviour
+    {
+        var gameObject = new GameObject(typeof(T).FullName);
+        var monoBehaviour = gameObject.AddComponent<T>();
+        monoBehaviour.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind<T>().ToConstant(monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnNewGameObject<T>(this INinjectModule self)
+        where T : MonoBehaviour
+        => BindMonoBehaviourOnNewGameObject<T>(self.Kernel!);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnNewGameObject(this IKernel self,
+        Type type)
+    {
+        if (!typeof(MonoBehaviour).IsAssignableFrom(type)) throw new BonejectException("Type is not a MonoBehaviour.");
+
+        var gameObject = new GameObject(type.FullName);
+        var monoBehaviour = gameObject.AddComponent(Il2CppType.From(type)) as MonoBehaviour;
+        monoBehaviour!.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind(type).ToConstant((object)monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnNewGameObject(this INinjectModule self,
+        Type type)
+        => BindMonoBehaviourOnNewGameObject(self.Kernel!, type);
+    
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnNewGameObject<T>(this IKernel self, out GameObject gameObject)
+        where T : MonoBehaviour
+    {
+        gameObject = new GameObject(typeof(T).FullName);
+        var monoBehaviour = gameObject.AddComponent<T>();
+        monoBehaviour.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind<T>().ToConstant(monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnNewGameObject<T>(this INinjectModule self, out GameObject gameObject)
+        where T : MonoBehaviour
+        => BindMonoBehaviourOnNewGameObject<T>(self.Kernel!, out gameObject);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnNewGameObject(this IKernel self,
+        Type type, out GameObject gameObject)
+    {
+        if (!typeof(MonoBehaviour).IsAssignableFrom(type)) throw new BonejectException("Type is not a MonoBehaviour.");
+
+        gameObject = new GameObject(type.FullName);
+        var monoBehaviour = gameObject.AddComponent(Il2CppType.From(type)) as MonoBehaviour;
+        monoBehaviour!.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind(type).ToConstant((object)monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnNewGameObject(this INinjectModule self,
+        Type type, out GameObject gameObject)
+        => BindMonoBehaviourOnNewGameObject(self.Kernel!, type, out gameObject);
+}
