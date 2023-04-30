856f93cc0e5330c69532ebdef0ef5c11f061a8bf
Millzy <millzyyt08@gmail.com>
2023-04-30 10:44:01 +0100

    switch to semver

--- Boneject/Mod.cs
+++ Boneject/Mod.cs
@@ -12,7 +12,8 @@ public static class BuildInfo
     public const string name = "Boneject";
     public const string author = "Millzy";
     public const string company = null!;
-    public const string version = "1.0.0";
+    public const string version = "1.0.0.0";
+    public const string semanticVersion = "1.0.0-pre.1";
     public const string downloadLink = null!;
 }
 
