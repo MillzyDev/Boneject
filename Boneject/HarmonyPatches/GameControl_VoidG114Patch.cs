fe12856bf94f5947a104c6e7b4e15319e46a7184
Millzy <millzyyt08@gmail.com>
2023-04-30 10:43:51 +0100

    void g114 context

---
+++ Boneject/HarmonyPatches/GameControl_VoidG114Patch.cs
@@ -0,0 +1,29 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using MelonLoader;
+using Ninject;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(GameControl_VoidG114))]
+[HarmonyPatch(nameof(GameControl_VoidG114.Start))]
+public static class GameControl_VoidG114Patch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once UnusedMember.Local
+    // ReSharper disable once InconsistentNaming
+    private static void Postfix(GameControl_VoidG114 __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+
+        var rigManager = __instance.rm;
+        var bodyVitals = __instance.ctrl_bodyVitals;
+
+        var baseModule = new VoidG114Module(bonejectManager, __instance, rigManager, bodyVitals);
+        kernel.Load(baseModule);
+        
+        MelonLogger.Msg("VoidG114 context loaded.");
+    }
+}
--- Boneject/Ninject/Modules/VoidG114Module.cs
+++ Boneject/Ninject/Modules/VoidG114Module.cs
@@ -1,6 +1,31 @@
-﻿namespace Boneject.Ninject.Modules;
+﻿using Ninject.Modules;
+using SLZ.Bonelab;
+using SLZ.Rig;
+using SLZ.VRMK;
 
-public class VoidG114Module
+namespace Boneject.Ninject.Modules;
+
+public class VoidG114Module : NinjectModule
 {
+    private readonly BonejectManager _bonejectManager;
+    private readonly GameControl_VoidG114 _gameControlVoidG114;
+    private readonly RigManager _rigManager;
+    private readonly BodyVitals _bodyVitals;
     
+    public VoidG114Module(BonejectManager bonejectManager, GameControl_VoidG114 gameControlVoidG114, RigManager rigManager, BodyVitals bodyVitals)
+    {
+        _bonejectManager = bonejectManager;
+        _gameControlVoidG114 = gameControlVoidG114;
+        _rigManager = rigManager;
+        _bodyVitals = bodyVitals;
+    }
+    
+    public override void Load()
+    {
+        Bind<GameControl_VoidG114>().ToConstant(_gameControlVoidG114).InSingletonScope();
+        Bind<RigManager>().ToConstant(_rigManager).InSingletonScope();
+        Bind<BodyVitals>().ToConstant(_bodyVitals).InSingletonScope();
+        
+        _bonejectManager.LoadForContext(this);
+    }
 }
