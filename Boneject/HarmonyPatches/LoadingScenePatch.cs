bfc662cc454be468f6bf891982889ea0dce99afb
Millzy <millzyyt08@gmail.com>
2023-04-30 09:29:00 +0100

    loading context

---
+++ Boneject/HarmonyPatches/LoadingScenePatch.cs
@@ -0,0 +1,26 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using MelonLoader;
+using Ninject;
+using SLZ.UI;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(LoadingScene))]
+[HarmonyPatch(nameof(LoadingScene.Start))]
+public static class LoadingScenePatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once UnusedMember.Local
+    // ReSharper disable once InconsistentNaming
+    private static void Postfix(LoadingScene __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+
+        var baseModule = new LoadingModule(bonejectManager, __instance);
+        kernel.Load(baseModule);
+        
+        MelonLogger.Msg("Loading context loaded.");
+    }
+}
--- Boneject/Ninject/Modules/LoadingModule.cs
+++ Boneject/Ninject/Modules/LoadingModule.cs
@@ -1,6 +1,23 @@
-﻿namespace Boneject.Ninject.Modules;
+﻿using Ninject.Modules;
+using SLZ.UI;
 
-public class LoadingModule
+namespace Boneject.Ninject.Modules;
+
+public class LoadingModule : NinjectModule
 {
+    private readonly BonejectManager _bonejectManager;
+    private readonly LoadingScene _loadingScene;
     
+    public LoadingModule(BonejectManager bonejectManager, LoadingScene loadingScene)
+    {
+        _bonejectManager = bonejectManager;
+        _loadingScene = loadingScene;
+    }
+    
+    public override void Load()
+    {
+        Bind<LoadingScene>().ToConstant(_loadingScene).InSingletonScope();
+        
+        _bonejectManager.LoadForContext(this);
+    }
 }
