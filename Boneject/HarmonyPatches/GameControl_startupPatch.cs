0a3bae65161814a49566577d33f035b6c11f45bf
Millzy <millzyyt08@gmail.com>
2023-04-30 10:43:30 +0100

    startup location

---
+++ Boneject/HarmonyPatches/GameControl_startupPatch.cs
@@ -0,0 +1,30 @@
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using MelonLoader;
+using Ninject;
+using SLZ.Bonelab;
+
+namespace Boneject.HarmonyPatches;
+
+[HarmonyPatch(typeof(GameControl_startup))]
+[HarmonyPatch(nameof(GameControl_startup.Start))]
+public static class GameControl_startupPatch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void Postfix(GameControl_startup __instance)
+    {
+        var bonejectManager = Mod.BonejectManager;
+        var kernel = bonejectManager.Kernel;
+
+        var controlPlayer = __instance.controlPlayer;
+        var bodyVitals = __instance.ctrl_bodyVitals;
+        var laserCursor = __instance.mainMenuUIController;
+
+        var baseModule = new StartupModule(bonejectManager, __instance, controlPlayer, bodyVitals, laserCursor);
+        kernel.Load(baseModule);
+        
+        MelonLogger.Msg("Startup context loaded.");
+    }
+}
--- Boneject/Ninject/Modules/StartupModule.cs
+++ Boneject/Ninject/Modules/StartupModule.cs
@@ -1,6 +1,35 @@
-﻿namespace Boneject.Ninject.Modules;
+﻿using Ninject.Modules;
+using SLZ.Bonelab;
+using SLZ.UI;
+using SLZ.VRMK;
 
-public class StartupModule
+namespace Boneject.Ninject.Modules;
+
+public class StartupModule : NinjectModule
 {
+    private readonly BonejectManager _bonejectManager;
+    private readonly GameControl_startup _gameControlStartup;
+    private readonly Control_Player _controlPlayer;
+    private readonly BodyVitals _bodyVitals;
+    private readonly LaserCursor _laserCursor;
     
+    public StartupModule(BonejectManager bonejectManager, GameControl_startup gameControlStartup, 
+        Control_Player controlPlayer, BodyVitals bodyVitals, LaserCursor laserCursor)
+    {
+        _bonejectManager = bonejectManager;
+        _gameControlStartup = gameControlStartup;
+        _controlPlayer = controlPlayer;
+        _bodyVitals = bodyVitals;
+        _laserCursor = laserCursor;
+    }
+    
+    public override void Load()
+    {
+        Bind<GameControl_startup>().ToConstant(_gameControlStartup).InSingletonScope();
+        Bind<Control_Player>().ToConstant(_controlPlayer).InSingletonScope();
+        Bind<BodyVitals>().ToConstant(_bodyVitals).InSingletonScope();
+        Bind<LaserCursor>().ToConstant(_laserCursor).InSingletonScope();
+        
+        _bonejectManager.LoadForContext(this);
+    }
 }
