ec59ec9f88e2995dad6544c726ba7d5a006fad2d
Millzy <millzyyt08@gmail.com>
2023-04-30 13:25:39 +0100

    BindMonoBehaviourOnGameObject methods

--- Boneject/Ninject/Extensions/UnityBindingExtensions.cs
+++ Boneject/Ninject/Extensions/UnityBindingExtensions.cs
@@ -48,6 +48,7 @@ public static class UnityBindingExtensions
         Type type)
         => BindMonoBehaviourOnNewGameObject(self.Kernel!, type);
     
+    // ReSharper disable once MemberCanBePrivate.Global
     public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnNewGameObject<T>(this IKernel self, out GameObject gameObject)
         where T : MonoBehaviour
     {
@@ -84,4 +85,40 @@ public static class UnityBindingExtensions
     public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnNewGameObject(this INinjectModule self,
         Type type, out GameObject gameObject)
         => BindMonoBehaviourOnNewGameObject(self.Kernel!, type, out gameObject);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnGameObject<T>(this IKernel self,
+        GameObject gameObject) where T : MonoBehaviour
+    {
+        var monoBehaviour = gameObject.AddComponent<T>();
+        monoBehaviour.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind<T>().ToConstant(monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<T> BindMonoBehaviourOnGameObject<T>(this INinjectModule self,
+        GameObject gameObject) where T : MonoBehaviour
+        => BindMonoBehaviourOnGameObject<T>(self.Kernel!, gameObject);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnGameObject(this IKernel self, Type type,
+        GameObject gameObject)
+    {
+        if (!typeof(MonoBehaviour).IsAssignableFrom(type)) throw new BonejectException("Type is not a MonoBehaviour.");
+        
+        var monoBehaviour = gameObject.AddComponent(Il2CppType.From(type)) as MonoBehaviour;
+        monoBehaviour!.enabled = false;
+        self.Inject(monoBehaviour);
+        var bindingSyntax = self.Bind(type).ToConstant((object)monoBehaviour);
+        monoBehaviour.enabled = true;
+        return bindingSyntax;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public static IBindingWhenInNamedWithOrOnSyntax<object> BindMonoBehaviourOnGameObject(this INinjectModule self,
+        Type type, GameObject gameObject)
+        => BindMonoBehaviourOnGameObject(self.Kernel!, type, gameObject);
 }
