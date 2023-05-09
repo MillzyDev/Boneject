df515c2841a84f647bfb4a4a40befd79eeedfb5c
Millzy <millzyyt08@gmail.com>
2023-05-09 19:13:07 +0100

    expression based field accessor

---
+++ Boneject/AccessUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Boneject;
+
+public static class AccessUtils
+{
+    public static Func<T, R> GetFieldAccessor<T, R>(string fieldName) 
+    { 
+        var param = Expression.Parameter(typeof(T),"arg");
+        var member = Expression.Field(param, fieldName);
+        var lambda = Expression.Lambda(typeof(Func<T,R>), member, param);
+        var compiled = (Func<T,R>)lambda.Compile(); 
+        return compiled; 
+    }
+}
