bc5e134a5d23beecdcab32fbbbd7d422e3866241
Millzy <millzyyt08@gmail.com>
2023-06-02 10:06:41 +0100

    BuildInfo code generation

---
+++ Boneject.CodeGen/SourceGeneratorContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Boneject.CodeGen
+{
+    public static class SourceGeneratorContextExtensions
+    {
+        public static string GetMSBuildProperty(this GeneratorExecutionContext context, string name, string defaultValue = "")
+        {
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{name}", out var value);
+            return value ?? defaultValue;
+        }
+    }
+}
