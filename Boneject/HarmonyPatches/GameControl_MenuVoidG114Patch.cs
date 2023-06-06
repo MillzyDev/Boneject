9313988f619dbe38c44a359d75b29b38b9b3c22d
Millzy <millzyyt08@gmail.com>
2023-06-06 20:46:08 +0100

    forgot to actually make it work

--- Boneject/HarmonyPatches/GameControl_MenuVoidG114Patch.cs
+++ Boneject/HarmonyPatches/GameControl_MenuVoidG114Patch.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using Boneject.Ninject.Modules;
+using HarmonyLib;
+using MelonLoader;
+using Ninject;
 using SLZ.Bonelab;
 
 namespace Boneject.HarmonyPatches;
@@ -9,6 +12,7 @@ public static class GameControl_MenuVoidG114Patch
 {
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Global
     public static void Postfix(GameControl_MenuVoidG114 __instance)
     {
         var bonejectManager = Mod.BonejectManager;
@@ -18,5 +22,11 @@ public static class GameControl_MenuVoidG114Patch
         var bodyVitals = __instance.ctrl_bodyVitals;
         var laserCursor = __instance.mainMenuUIController;
         var fadeVolume = __instance.fadeVolume;
+
+        var baseModule = new VoidG114MenuModule(bonejectManager, __instance, controlPlayer, bodyVitals, laserCursor,
+            fadeVolume);
+        kernel.Load(baseModule);
+        
+        MelonLogger.Msg("VoidG114Menu context loaded.");
     }
 }
