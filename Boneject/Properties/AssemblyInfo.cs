fb1ee5367ff33892a57b04377c86b89546dae268
Millzy <millzyyt08@gmail.com>
2023-06-06 18:10:17 +0100

    fix broken loader version to verify

--- Boneject/Properties/AssemblyInfo.cs
+++ Boneject/Properties/AssemblyInfo.cs
@@ -3,27 +3,28 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using Boneject;
 using MelonLoader;
+using BuildInfo = Boneject.BuildInfo;
 
-[assembly: AssemblyTitle(Boneject.BuildInfo.name)]
+[assembly: AssemblyTitle(BuildInfo.name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany(null!)]
-[assembly: AssemblyProduct(Boneject.BuildInfo.name)]
+[assembly: AssemblyProduct(BuildInfo.name)]
 [assembly: AssemblyCopyright("Copyright (c) 2023 Frederick (Millzy) Mills")]
 [assembly: AssemblyTrademark(null)]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion(Boneject.BuildInfo.version)]
-[assembly: AssemblyFileVersion(Boneject.BuildInfo.semVersion)]
+[assembly: AssemblyVersion(BuildInfo.version)]
+[assembly: AssemblyFileVersion(BuildInfo.semVersion)]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: MelonInfo(typeof(Mod), Boneject.BuildInfo.name, Boneject.BuildInfo.semVersion, Boneject.BuildInfo.author, 
-    Boneject.BuildInfo.downloadLink)]
+[assembly: MelonInfo(typeof(Mod), BuildInfo.name, BuildInfo.semVersion, BuildInfo.author, 
+    BuildInfo.downloadLink)]
 
 [assembly: MelonPriority(-1000000)]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 [assembly: MelonID("{5}")]
 
-[assembly: VerifyLoaderVersion("{6}")]
+[assembly: VerifyLoaderVersion(BuildInfo.melonLoaderVersion)]
 
 [assembly: HarmonyDontPatchAll]
