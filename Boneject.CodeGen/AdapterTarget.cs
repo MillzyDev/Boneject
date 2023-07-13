4587412c323c41cc9dec4350df35af63be823b3a
Millzy <millzyyt08@gmail.com>
2023-07-13 22:24:40 +0100

    group

---
+++ Boneject.CodeGen/AdapterTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Boneject.CodeGen
+{
+    public struct AdapterTarget
+    {
+        public readonly Assembly Assembly;
+        public readonly string Class;
+
+        public AdapterTarget(Assembly assembly, string @class)
+        {
+            Assembly = assembly;
+            Class = @class;
+        }
+    }
+}
