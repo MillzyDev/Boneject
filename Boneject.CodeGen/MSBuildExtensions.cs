e8c748911cd2a3a9fa8e31b12a8c19d5c42d82e6
Millzy <millzyyt08@gmail.com>
2023-07-14 15:56:06 +0100

    get MSBuild properties

---
+++ Boneject.CodeGen/MSBuildExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Boneject.CodeGen
+{
+    public static class MSBuildExtensions
+    {
+        public static string GetMSBuildProperty(this GeneratorExecutionContext context, string name, 
+                                                string defaultValue = "")
+        {
+            context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{name}", out string? value);
+            return value ?? defaultValue;
+        }
+    }
+}
