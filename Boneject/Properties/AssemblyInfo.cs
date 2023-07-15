6d0088c003b9fe70b87f327a296543765f774532
Millzy <millzyyt08@gmail.com>
2023-07-15 16:37:22 +0100

    version bump

--- Boneject/Properties/AssemblyInfo.cs
+++ Boneject/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using MelonLoader;
 [assembly: AssemblyTrademark(null)]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: MelonInfo(typeof(Mod), nameof(Boneject), "1.2.0", "Millzy",
+[assembly: MelonInfo(typeof(Mod), nameof(Boneject), "2.0.0", "Millzy",
     "https://github.com/MillzyDev/Boneject/releases/latest/download/Boneject.zip")]
 
 [assembly: MelonPriority(-1000000)]
