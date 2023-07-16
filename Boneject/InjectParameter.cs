da6f3024c81236b5b62baefa8992bf5a4962221a
Millzy <millzyyt08@gmail.com>
2023-07-16 14:26:19 +0100

    rewrite

--- Boneject/InjectParameter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using MelonLoader;
-
-namespace Boneject
-{
-    public delegate object? InjectParameter(object? previous, ParameterInfo? parameter,
-                                            MelonInfoAttribute info);
-}
