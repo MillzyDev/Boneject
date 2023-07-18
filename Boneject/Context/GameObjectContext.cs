67927ab0c5cf4704690d9962c6b1e49618240617
Millzy <millzyyt08@gmail.com>
2023-07-18 10:26:46 +0100

    empty behaviours

---
+++ Boneject/Context/GameObjectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace Boneject.Context
+{
+    [RegisterTypeInIl2Cpp]
+    public class GameObjectContext : MonoBehaviour
+    {
+        public GameObjectContext(IntPtr ptr) : base(ptr)
+        {
+        }
+    }
+}
---
+++ Boneject/Context/SceneContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace Boneject.Context
+{
+    [RegisterTypeInIl2Cpp]
+    public class SceneContext : MonoBehaviour
+    {
+        public SceneContext(IntPtr ptr) : base(ptr)
+        {
+        }
+    }
+}
