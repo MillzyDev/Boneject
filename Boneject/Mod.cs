47373cb16a2233509c6bbb9e52c44d7259c7fbd7
Millzy <millzyyt08@gmail.com>
2023-07-18 21:53:25 +0100

    setup contexts

--- Boneject/Mod.cs
+++ Boneject/Mod.cs
@@ -1,8 +1,35 @@
-﻿using MelonLoader;
+﻿using Boneject.Context;
+using MelonLoader;
+using UnityEngine;
 
 namespace Boneject
 {
     public class Mod : MelonMod
     {
+        
+        
+        // Start()
+        public override void OnLateInitializeMelon()
+        {
+            var appContextObject = new GameObject("BonejectApplicationContext");
+            appContextObject.SetActive(false);
+            
+            var appContext = appContextObject.AddComponent<SceneContext>();
+            
+            Object.DontDestroyOnLoad(appContextObject);
+            appContextObject.SetActive(true);
+            
+            
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            var sceneContextObject = new GameObject("BonejectSceneContext");
+            sceneContextObject.SetActive(false);
+            
+            var sceneContext = sceneContextObject.AddComponent<SceneContext>();
+            
+            sceneContextObject.SetActive(true);
+        }
     }
 }
