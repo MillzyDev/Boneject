a276d9267cf0073937df571b4a256db7d912c45b
Millzy <millzyyt08@gmail.com>
2023-07-18 10:26:40 +0100

    bonejector that does nothing

---
+++ Boneject/Bonejector.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+namespace Boneject
+{
+    public class Bonejector
+    {
+        private MelonInfoAttribute _info;
+        
+        private Bonejector(MelonInfoAttribute info)
+        {
+            _info = info;
+        }
+
+        public static Bonejector Get(MelonInfoAttribute info)
+        {
+            var bonejector = new Bonejector(info);
+            // TODO: Register in BonejectManager
+            return bonejector;
+        }
+    }
+}
