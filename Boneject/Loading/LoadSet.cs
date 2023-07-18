a35a0b3669c5e611dee33a549efb30beb05cd2da
Millzy <millzyyt08@gmail.com>
2023-07-18 10:25:43 +0100

    load sets

---
+++ Boneject/Loading/LoadSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Boneject.Filters;
+
+namespace Boneject.Loading
+{
+    public class LoadSet
+    {
+        public readonly Type ModuleType;
+        public readonly ILoadFilter LoadFilter;
+        public readonly object[]? InitialArguments;
+
+        public LoadSet(Type moduleType, ILoadFilter loadFilter, object[]? initialArguments = null)
+        {
+            ModuleType = moduleType;
+            LoadFilter = loadFilter;
+            InitialArguments = initialArguments;
+        }
+    }
+}
---
+++ Boneject/Loading/SceneLoadSet.cs
@@ -0,0 +1,18 @@
+﻿using Boneject.Filters;
+
+namespace Boneject.Loading
+{
+    public struct SceneLoadSet
+    {
+        public readonly string SceneName;
+        public readonly ISceneLoadFilter LoadFilter;
+        public readonly object[]? InitialArguments;
+
+        public SceneLoadSet(string sceneName, ISceneLoadFilter loadFilter, object[]? initialArguments = null)
+        {
+            SceneName = sceneName;
+            LoadFilter = loadFilter;
+            InitialArguments = initialArguments;
+        }
+    }
+}
