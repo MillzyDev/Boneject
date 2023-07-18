fa524359611485e42d71b4e666262311e1131f47
Millzy <millzyyt08@gmail.com>
2023-07-18 10:26:22 +0100

    locations

---
+++ Boneject/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Boneject
+{
+    [Flags]
+    public enum Location
+    {
+        None = 0,
+        
+        App = 1,
+        
+        Bootstrapper = 2,
+        
+        Loading = 4,
+        
+        Player = 8,
+        
+        Startup = 16,
+        
+        VoidG114Menu = 32,
+        
+        Menu = Startup | VoidG114Menu
+    }
+}
