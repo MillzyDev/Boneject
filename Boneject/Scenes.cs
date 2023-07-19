c1cbfefc56b84c2554df09bca6eac6d7423b28a0
Millzy <millzyyt08@gmail.com>
2023-07-19 18:14:29 +0100

    empty scene constant helpers

---
+++ Boneject/Scenes.cs
@@ -0,0 +1,10 @@
+﻿namespace Boneject
+{
+    public static class Scenes
+    {
+        public const string Bootstrapper = "";
+        public const string Loading = "";
+        public const string Startup = "";
+        public const string VoidG114Menu = "";
+    }
+}
