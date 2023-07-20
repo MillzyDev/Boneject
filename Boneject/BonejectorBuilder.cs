b037fd63c8e482ad36192e3aaccb3572a13d909b
Millzy <millzyyt08@gmail.com>
2023-07-20 18:23:36 +0100

    created builder to comply with srp

---
+++ Boneject/BonejectorBuilder.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+
+namespace Boneject
+{
+    public class BonejectorBuilder
+    {
+        private readonly MelonInfoAttribute _melonInfo;
+        
+        public BonejectorBuilder(MelonInfoAttribute melonInfo)
+        {
+            _melonInfo = melonInfo;
+        }
+
+        public Bonejector Build() => new Bonejector(_melonInfo);
+    }
+}
