ba8e641720a495258c09a068e8e2c2f4910bdb1f
Millzy <millzyyt08@gmail.com>
2023-07-20 23:39:46 +0100

    basic scene context

--- Boneject/Context/SceneContext.cs
+++ Boneject/Context/SceneContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MelonLoader;
+using Ninject;
 using UnityEngine;
 
 namespace Boneject.Context
@@ -7,8 +8,23 @@ namespace Boneject.Context
     [RegisterTypeInIl2Cpp]
     public class SceneContext : MonoBehaviour
     {
+        [NonSerialized]
+        public string ContractName = null!;
+        public KernelConfiguration Kernel = null!;
+        public BonejectManager BonejectManager = null!;
+        
         public SceneContext(IntPtr ptr) : base(ptr)
         {
+        }
+
+        private void Awake()
+        {
+            ContractName = gameObject.scene.name;
+        }
+
+        public void Start()
+        {
+            
         }
     }
 }
